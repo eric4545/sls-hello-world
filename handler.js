0ac63ff0df03fbfe012d539116aa548e19a39d41
Eric N <eric4545@gmail.com>
2018-06-26 00:10:57 +0800

    :tada: Initial commit generate from sls

---
+++ handler.js
@@ -0,0 +1,25 @@
+'use strict';
+
+exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
+
+exports.webhook = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
